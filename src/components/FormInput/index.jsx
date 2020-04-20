6a98cc9b68ea7ed480abfe2cfcceceebf79cd648
Ellis Min <dbmin@hotmail.com>
2020-04-20 05:39:07 -0500

    [UPDATE]: uses fixed font size for input

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -13,23 +13,23 @@ const FormInput = ({
   handleRemove,
   ...otherProps
 }) => {
-  const addChild = e => {
+  const addChild = (e) => {
     e.preventDefault();
     const newChild = {
       name: `child-${childCount + 1}`,
       level: child.level,
       value: "",
-      subChildren: []
+      subChildren: [],
     };
     handleAddChild(newChild);
   };
 
-  const addSubChild = e => {
+  const addSubChild = (e) => {
     e.preventDefault();
     handleAddSubChild(child);
   };
 
-  const removeChild = e => {
+  const removeChild = (e) => {
     e.preventDefault();
     handleRemove(child.name);
   };
@@ -39,18 +39,18 @@ const FormInput = ({
   return (
     <div className="form-group" style={{ marginLeft: marginLeft }}>
       <label>{label}</label>
-      <input type="text" {...otherProps} />
+      <input className="input-custom" type="text" {...otherProps} />
       <div className="btn-container">
         {label !== "root" && (
           <>
-            <Button onClick={e => addSubChild(e)}>↓</Button>
+            <Button onClick={(e) => addSubChild(e)}>↓</Button>
             {label !== "child-1" && (
-              <Button className="btn-delete" onClick={e => removeChild(e)}>
+              <Button className="btn-delete" onClick={(e) => removeChild(e)}>
                 -
               </Button>
             )}
             {lastChild && (
-              <Button className="btn-add" onClick={e => addChild(e)}>
+              <Button className="btn-add" onClick={(e) => addChild(e)}>
                 +
               </Button>
             )}
